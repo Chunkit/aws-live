981226d625eace430b9b99d8564d7cb410e51830
Chunkit <141215935+Chunkit@users.noreply.github.com>
2023-09-04 09:28:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c9euwctn4e9a.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "kwochunkit-employee"
+customregion = "us-east-1a"
+
